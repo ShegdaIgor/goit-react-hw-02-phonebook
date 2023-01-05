afdae4732bb15d12b73566029919f18566267c68
Shegda Igor <igorbeef@gmail.com>
2023-01-05 14:23:46 +0200

    remove empty fragment

--- src/components/ContactsList/ContactsList.jsx
+++ src/components/ContactsList/ContactsList.jsx
@@ -8,29 +8,27 @@ export default class ContactsList extends Component {
   render() {
     const { showFilteredContacts, handleDeleteContact } = this.props;
     return (
-      <>
-        <ul className={css.contactList}>
-          {showFilteredContacts.length ? (
-            showFilteredContacts.map(contact => (
-              <li key={contact.id} className={css.contactItem}>
-                <p className={css.contactInfoWrapper}>
-                  <span className={css.contactName}>{contact.name}:</span>&nbsp;
-                  <span className={css.contactNumber}>{contact.number}</span>
-                </p>
+      <ul className={css.contactList}>
+        {showFilteredContacts.length ? (
+          showFilteredContacts.map(contact => (
+            <li key={contact.id} className={css.contactItem}>
+              <p className={css.contactInfoWrapper}>
+                <span className={css.contactName}>{contact.name}:</span>&nbsp;
+                <span className={css.contactNumber}>{contact.number}</span>
+              </p>
 
-                <DeleteBtn
-                  type="button"
-                  onDeleteContact={handleDeleteContact}
-                  id={contact.id}
-                  actionText=" Delete"
-                ></DeleteBtn>
-              </li>
-            ))
-          ) : (
-            <ContactsInfo />
-          )}
-        </ul>
-      </>
+              <DeleteBtn
+                type="button"
+                onDeleteContact={handleDeleteContact}
+                id={contact.id}
+                actionText=" Delete"
+              ></DeleteBtn>
+            </li>
+          ))
+        ) : (
+          <ContactsInfo />
+        )}
+      </ul>
     );
   }
 }
